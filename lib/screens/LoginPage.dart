155e2a8321196740cde3396e7b98e3bb0cf23dff
Intan Nur Shahira <intanshahiraa@gmail.com>
2020-04-11 23:45:12 +0800

    Added LoginPage.dart

---
+++ lib/screens/LoginPage.dart
@@ -0,0 +1,155 @@
+//import 'package:bronco2/HomePage/HomePage.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xff21254A),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              height: 200,
+              child: Stack(children: <Widget>[
+                Positioned(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/2.gif"),
+                )))),
+              ])),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "BRONCO AIRLINES \nWelcomes you back",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.transparent,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.grey[100],
+                            ),
+                          ),
+                        ),
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.grey[100],
+                            ),
+                          ),
+                        ),
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Center(
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: Colors.pink[200],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                FlatButton(
+                    color: Color.fromRGBO(33, 37, 74, 1),
+                    onPressed: () {
+                     // navigateToHomePage(context);
+                    },
+                    child: Container(
+                      height: 50,
+                      margin: EdgeInsets.symmetric(horizontal: 60),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.pink[200],
+                              offset: Offset(0.0, 1.0),
+                              blurRadius: 6.0),
+                        ],
+                        color: Color.fromRGBO(49, 39, 79, 1),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    )),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Center(
+                  child: Text(
+                    "Create Account",
+                    style: TextStyle(
+                      color: Colors.pink[200],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//Future navigateToHomePage(context) async {
+  //Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+//}
