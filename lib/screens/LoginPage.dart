03c2799cada05aeed4b057a7421c49ffc0a67ac0
Fitri Nabiha <fitri.nabiha15@gmail.com>
2020-05-09 16:48:23 +0800

    update login page

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:bronco2/HomePage/HomePage.dart';
+//import 'package:bronco2/screens/SignUpPage.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -7,6 +8,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,10 +30,13 @@ class _LoginPageState extends State<LoginPage> {
                 )))),
               ])),
           SizedBox(
-            height: 20,
+            height: 5,
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -51,44 +57,62 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.circular(10.0),
                     color: Colors.transparent,
                   ),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey[100],
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.grey[100],
+                              ),
                             ),
                           ),
-                        ),
-                        child: TextField(
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Email",
-                            hintStyle: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey[100],
-                            ),
-                          ),
-                        ),
-                        child: TextField(
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return "Email can't be empty";
+                              } else {}
+                              return null;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
                               border: InputBorder.none,
-                              hintText: "Password",
-                              hintStyle: TextStyle(color: Colors.grey)),
+                              hintText: "Email Address",
+                              hintStyle: TextStyle(color: Colors.white),
+                            ),
+                          ),
                         ),
-                      )
-                    ],
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.grey[100],
+                              ),
+                            ),
+                          ),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return "Password can't be empty";
+                              }
+
+                              return null;
+                            },
+                            obscureText: true,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey)),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -108,7 +132,10 @@ class _LoginPageState extends State<LoginPage> {
                 FlatButton(
                     color: Color.fromRGBO(33, 37, 74, 1),
                     onPressed: () {
-                      navigateToHomePage(context);
+                      _formKey.currentState.validate()
+                          ? showDialogBox(context)
+                          : Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Not valid!')));
                     },
                     child: Container(
                       height: 50,
@@ -133,11 +160,16 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(
                   height: 20.0,
                 ),
-                Center(
-                  child: Text(
-                    "Create Account",
-                    style: TextStyle(
-                      color: Colors.pink[200],
+                new InkWell(
+                  onTap: () {
+                    navigateToSignUpPage(context);
+                  },
+                  child: Center(
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(
+                        color: Colors.pink[200],
+                      ),
                     ),
                   ),
                 ),
@@ -152,4 +184,55 @@ class _LoginPageState extends State<LoginPage> {
 
 Future navigateToHomePage(context) async {
   Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+}
+
+Future navigateToSignUpPage(context) async {
+ // Navigator.push(
+      //context, MaterialPageRoute(builder: (context) => SignUpPage()));
+}
+
+showDialogBox(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    child: AlertDialog(
+      backgroundColor: Color.fromRGBO(49, 39, 79, 1),
+      title: Text('Login Successful',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white)),
+      content: Text('Welcome to Bronco Airlines!',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white)),
+      actions: <Widget>[
+        Center(
+          child: InkWell(
+            onTap: () {
+              navigateToHomePage(context);
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 22.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                  bottomRight: Radius.circular(25.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.pink[200],
+                      offset: Offset(0.0, 1.0),
+                      blurRadius: 6.0),
+                ],
+                color: Color.fromRGBO(49, 39, 79, 1),
+              ),
+              child: Text(
+                "Continue",
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
