82992b47ee4426887c6d323ac7b06e801d354aeb
Fitri Nabiha <fitri.nabiha15@gmail.com>
2020-05-09 21:51:02 +0800

    Added flight_stop_ticket.dart

---
+++ lib/Booking/flight_stop_ticket.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class FlightStopTicket{
+  String from;
+  String fromShort;
+  String to;
+  String toShort;
+  String flightNumber;
+  String date;
+  //bool isClickable;
+
+
+  FlightStopTicket(this.from,this.fromShort, this.to, this.toShort,
+  this.flightNumber, this.date/*, @required this.isClickable*/);
+}
