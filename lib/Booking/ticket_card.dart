08b899780fabe58e5346174e0d53b70a5ce7f6ce
Nurul Nabilah <bella_e29@yahoo.com>
2020-07-26 09:16:34 +0800

    delete ticket card

--- lib/Booking/ticket_card.dart
+++ lib/Booking/ticket_card.dart
@@ -1,31 +1,42 @@
 import 'package:bronco2/Booking/booking_page.dart';
 import 'package:bronco2/Payment/boardingPass.dart';
+import 'package:flutter/cupertino.dart';
 import 'flight_stop_ticket.dart';
 import 'package:bronco2/Booking/flight_stop_ticket.dart';
 import 'package:flutter/material.dart';
 
 class TicketCard extends StatelessWidget {
-  get context => null;
+  get context => stop;
 
-  @override
   TicketCard createState() => new TicketCard();
-
-  final FlightStopTicket stop;
-
   const TicketCard({Key key, this.stop}) : super(key: key);
+  final FlightStopTicket stop;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 10.0,
-      margin: const EdgeInsets.all(2.0),
-      child: InkWell(
-        onTap: () {
-          navigateToBoardingPass(context);
-        },
-        child: _buildCardContent(),
-      ),
-    );
+    return Dismissible(
+        key: new ObjectKey(stop),
+        child: new Card(
+            elevation: 10.0,
+            margin: const EdgeInsets.all(2.0),
+            child: InkWell(
+              onTap: () {
+                navigateToBoardingPass(context);
+              },
+              child: _buildCardContent(),
+            )),
+        confirmDismiss: (DismissDirection dismissDirection) async {
+          switch (dismissDirection) {
+            case DismissDirection.startToEnd:
+              return await _showConfirmationDialog(context, 'delete') == true;
+            case DismissDirection.horizontal:
+            case DismissDirection.vertical:
+            case DismissDirection.up:
+            case DismissDirection.down:
+              assert(false);
+          }
+          return false;
+        });
   }
 
   Container _buildCardContent() {
@@ -39,116 +50,97 @@ class TicketCard extends StatelessWidget {
         new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500);
 
     TextStyle flightDate =
-        new TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500);
+        new TextStyle(fontSize: 13.0, fontWeight: FontWeight.w500);
 
     return Container(
       height: 180.0,
-      
       child: Column(
         children: <Widget>[
           Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            
-            child: FlatButton(
-              onPressed: () {
-                navigateToBoardingPass(context);
-              }, 
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40.0, top: 25.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(stop.from, style: airportName),
-                  ),
-                  Text(stop.fromShort, style: airportShortName),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: Text(stop.date, style: flightDate),
-                  )
-                ],
-                ),
-                )
-                )
-            /*Padding(
-              padding: const EdgeInsets.only(left: 40.0, top: 25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(stop.from, style: airportName),
-                  ),
-                  Text(stop.fromShort, style: airportShortName),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: Text(stop.date, style: flightDate),
-                  )
-                ],
-              ),
-            ),*/
-            
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Icon(
-                  Icons.airplanemode_active,
-                  color: Colors.pinkAccent,
-                ),
-              ),
-             Text(stop.flightNumber, style: flightNum),
-            ],
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 35.0, top: 25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        navigateToBoardingPass(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15.0, top: 25.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(stop.from, style: airportName),
+                            ),
+                            Text(stop.fromShort, style: airportShortName),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: Text(stop.date, style: flightDate),
+                            )
+                          ],
+                        ),
+                      ))),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(stop.to, style: airportName),
+                    padding: const EdgeInsets.only(top: 25, bottom: 5.0),
+                    child: Icon(
+                      Icons.airplanemode_active,
+                      color: Colors.pinkAccent,
+                    ),
                   ),
-                  Text(stop.toShort, style: airportShortName),
-                  Padding(
-                      padding: const EdgeInsets.only(top: 5.0),
-                      child: new FlatButton(
-                        color: Colors.transparent,
-                       
-                        onPressed: _showDialog,
-
-                        child: Text('Cancel Booking',
-                            style: TextStyle(
-                              color: Colors.red,
-                            )),
-                      )),
+                  Text(stop.flightNumber, style: flightNum),
                 ],
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 35.0, top: 30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(stop.to, style: airportName),
+                      ),
+                      Text(stop.toShort, style: airportShortName),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: new Text('Swipe to Cancel Booking',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 12.0,
+                              ))),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
-        ],
-      ),
-      
-      
     );
   }
 
-  /*Future<bool> _showConfirmationDialog(BuildContext context, String action) {
+  Future navigateToBoardingPass(context) async {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => BoardingPass()));
+  }
+
+  Future<bool> _showConfirmationDialog(BuildContext context, String action) {
     return showDialog<bool>(
       context: context,
       barrierDismissible: true,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Do you want to $action this item?'),
+          backgroundColor: Color.fromRGBO(49, 39, 79, 1),
+          title: Text('Do you confirm to cancel this booking?',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white)),
           actions: <Widget>[
             FlatButton(
               child: const Text('Yes'),
@@ -166,32 +158,5 @@ class TicketCard extends StatelessWidget {
         );
       },
     );
-  }*/
-
-  void _showDialog() {
-    // flutter defined function
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Alert Dialog title"),
-          content: new Text("Alert Dialog body"),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
-}
-Future navigateToBoardingPass(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => BoardingPass()));
 }
