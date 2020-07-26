e471c016a79d2cca976261e6e97c1ec527d51910
Nurul Nabilah <bella_e29@yahoo.com>
2020-07-26 12:16:20 +0800

    update booking_page.dart

--- lib/Booking/booking_page.dart
+++ lib/Booking/booking_page.dart
@@ -18,65 +18,61 @@ Color secondColor = Color.fromRGBO(
   1,
 );
 
-class BookingPage extends StatefulWidget{
+class BookingPage extends StatefulWidget {
   @override
   _BookingPageState createState() => _BookingPageState();
 }
 
-class _BookingPageState extends State<BookingPage> with TickerProviderStateMixin{
-
+class _BookingPageState extends State<BookingPage>
+    with TickerProviderStateMixin {
   List<FlightStopTicket> stops = [
-    new FlightStopTicket("Boston", "BOS", "Kuala Lumpur", "KUL", "KU2342", "24 June 2019"),
-    new FlightStopTicket("Kuala Lumpur", "KUL", "Boston", "BOS", "KU2341", "24 June 2019"),
+    new FlightStopTicket(
+        "Boston", "BOS", "Kuala Lumpur", "KUL", "KU2342", "24 June 2019"),
+    new FlightStopTicket(
+        "Kuala Lumpur", "KUL", "Boston", "BOS", "KU2341", "24 June 2019"),
   ];
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
             icon: Icon(Icons.arrow_back_ios),
-            onPressed:(){
+            onPressed: () {
+              Navigator.pop(context);
               //navigateToFlightSUmmary(context);
               //navigateToBoardingPass(context);
-              navigateToSettings(context);
-            }
-        ),
+              //navigateToSettings(context);
+            }),
         title: Text(
           'My Booking',
           style: TextStyle(
               fontFamily: 'Comfortaa',
               fontSize: 25.0,
-              fontWeight: FontWeight.bold
-          ),
+              fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         backgroundColor: Color(0xff21254A),
-          ),
-        resizeToAvoidBottomInset: true,
-         backgroundColor: Color(0xff21254A),
+      ),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Color(0xff21254A),
       body: new Stack(
-
         children: <Widget>[
-
-
           Positioned.fill(
-              top: MediaQuery.of(context).padding.top+64.0,
+              top: MediaQuery.of(context).padding.top + 64.0,
               child: SingleChildScrollView(
                 child: new Column(
                   children: _buildTickets().toList(),
-                  
                 ),
               ))
         ],
-
       ),
       /*floatingActionButton: _buildFab(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,*/
     );
   }
-  Iterable<Widget> _buildTickets(){
-    return stops.map((stop){
+
+  Iterable<Widget> _buildTickets() {
+    return stops.map((stop) {
       return Padding(
         padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
         child: TicketCard(stop: stop),
@@ -91,8 +87,9 @@ class _BookingPageState extends State<BookingPage> with TickerProviderStateMixin
     );
   }*/
 
-
 }
+
 Future navigateToBoardingPass(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => BoardingPass()));
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => BoardingPass()));
 }
